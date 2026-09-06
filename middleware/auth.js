4ec434a71febeb53efcbaa4978e82625d7e63cc8
An Nguyen <anqnguyen3@gmail.com>
2026-09-05 20:53:18 -0500

    assignment8

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function authMiddleware(req, res, next) {
-  if (!global.user_id) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  next();
-}
-
-module.exports = authMiddleware;
